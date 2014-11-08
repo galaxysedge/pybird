20b4fbc885a84f31582c0fc81f13c917814f2373
galaxysedge <dasha.barsky@googlemail.com>
2014-11-08 15:12:34 +0000

    Added two surfaces
    added background surface and bird surface, stopped redrawing pipes, added restart and quit key buttons

--- bird.py
+++ bird.py
@@ -5,90 +5,118 @@ from ipdb import set_trace
 
 pygame.init()
 
-black=(0,0,0)
-white=(255,255,255)
-red=(255,0,0)
-size_x=600
-size_y=400
-offsetx=50
-offsety=50
-size=[size_x+offsetx*2,size_y+offsety*2]
-screen=pygame.display.set_mode(size)
 
 
-g=9.81*3
-dt=0.05
-vy=0
-vx=20
-x=0
-y=0
-height=200
-bounce=-50
-pipedistance=120
-pipewidth=50
-pipegap=120
-pipes=[]
-prev = None
+def mainloop():
+    black=(0,0,0)
+    white=(255,255,255)
+    red=(255,0,0)
+    size_x=600
+    size_y=400
+    offsetx=50
+    offsety=50
+    size=[size_x+offsetx*2,size_y+offsety*2]
+    screen=pygame.display.set_mode(size)
 
-for i in range((size_x/pipedistance)):
-    j=i+1
-    bottom_edge=random.randint(0,size_y-pipegap)
-    left_edge=j*pipedistance-(pipewidth/2)
-    top_edge=bottom_edge+pipegap
-    right_edge=left_edge+pipewidth
-    pipes+=[(bottom_edge,top_edge,left_edge,right_edge)]
 
-label=None
-done=False
-font=pygame.font.Font(None, 30)
+    g=9.81/5
+    dt=0.05
+    vy=0
+    vx=6
+    x=0
+    y=0
+    height=200
+    bounce=-10
+    pipedistance=120
+    pipewidth=50
+    pipegap=120
+    pipes=[]
+    birdsize=5
+    prev = None
 
-screen.fill(white)
+    for i in range((size_x/pipedistance)):
+        j=i+1
+        bottom_edge=random.randint(0,size_y-pipegap)
+        left_edge=j*pipedistance-(pipewidth/2)
+        top_edge=bottom_edge+pipegap
+        right_edge=left_edge+pipewidth
+        pipes+=[(bottom_edge,top_edge,left_edge,right_edge)]
 
-window=pygame.Surface((size_x,size_y))
-window.fill(white)
-pygame.draw.rect(window, red, (0,0,size_x,size_y), 3)
-moving = True
-control = True
+    label=None
+    font=pygame.font.Font(None, 30)
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN:
-            #set_trace()
-            if event.key == 32 and control:
-                vy = bounce
+    screen.fill(white)
+    window=pygame.Surface((size_x,size_y))
+    bg=pygame.Surface((size_x,size_y))
+    bg.fill(white)
+    pygame.draw.rect(bg, red, (0,0,size_x,size_y), 3)
+    moving = True
+    control = True
+    bg.fill(white)
+    pygame.draw.rect(bg, red, (0,0,size_x,size_y), 3)
 
-    if moving: 
-        x += dt*vx
-        y += dt*vy
-        vy += g*dt
-
-    if x>=size_x or x<=0 or y>=size_y:
-        moving = False
-        vx = 0
-        vy = 0
-
-    window.fill(white)
-    pygame.draw.circle(window,red,[int(x),int(y)],5)        
-    pygame.draw.rect(window, red, (0,0,size_x,size_y), 3)
-
-    #generating pipes
-    #tuple in pipes: (bottom_edge,top_edge,left_edge,right_edge)
     for i in pipes:
-        pygame.draw.rect(window,black,[i[2],i[1],pipewidth,size_y-i[1]])
-        pygame.draw.rect(window,black,[i[2],0,pipewidth,i[0]])
-        if i[2]<x<i[3] and (i[1]<y<size_y or 0<y<i[0]):
-            control = False
-
-    screen.blit(window,(offsetx,offsety))    
-    pygame.display.flip()
-    cur = time.time()
-    if prev:
-        delay = 1.0/25 - (cur-prev)
-        if delay > 0:
-            time.sleep(delay)
-
-    prev = time.time()
+        pygame.draw.rect(bg,black,[i[2],i[1],pipewidth,size_y-i[1]])
+        pygame.draw.rect(bg,black,[i[2],0,pipewidth,i[0]])
     
-pygame.quit()
+    bird=pygame.Surface((birdsize*2,birdsize*2), pygame.SRCALPHA)
+    bird.fill( (255,255,255,0) )
+    pygame.draw.circle(bird,red,[birdsize,birdsize],birdsize)
+
+    
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.KEYDOWN:
+                #set_trace()
+                if event.key == 27: #esc
+                    return False
+                if event.unicode == ' ' and control:
+                    vy = bounce
+                if event.unicode == 'r':
+                    return True
+
+        if moving: 
+            x += dt*vx
+            y += dt*vy
+            vy += g*dt
+
+        if x>=size_x or x<=0 or y>=size_y:
+            moving = False
+            vx = 0
+            vy = 0
+
+
+        #generating pipes
+        #tuple in pipes: (bottom_edge,top_edge,left_edge,right_edge)
+        for i in pipes:
+            if i[2]<x<i[3] and (i[1]<y<size_y or 0<y<i[0]):
+                control = False
+
+        window.blit(bg,(0,0))
+        window.blit(bird,(x-birdsize,y-birdsize))
+        screen.blit(window,(offsetx,offsety))
+        
+        pygame.display.flip()
+        cur = time.time()
+        if prev:
+            delay = 1.0/100 - (cur-prev)
+            if delay > 0:
+                time.sleep(delay)
+
+        prev = time.time()
+
+
+def game():
+    while mainloop():
+        pass
+    
+    pygame.quit()
+
+def main():
+    game()
+
+if __name__ == "__main__":
+    main()
